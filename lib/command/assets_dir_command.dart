2170e223e8c6c8ac7d1912fa16d56ba80fbd2ea3
zengfeng <903600017@qq.com>
2023-08-19 22:44:50 +0800

    修改path路径生成'

--- lib/command/assets_dir_command.dart
+++ lib/command/assets_dir_command.dart
@@ -8,6 +8,10 @@ import 'package:pubspec_parse/pubspec_parse.dart';
 import 'package:yaml/yaml.dart';
 
 class AssetsDirCommand extends Command<String> {
+  static const _assets = "assets/";
+  static const _assets_images = "${_assets}images/";
+  static const _assets_audio = "${_assets}audio/";
+
   @override
   String get description => "Generate the Assets dir class.";
 
@@ -77,6 +81,44 @@ class AssetsDirCommand extends Command<String> {
         ..modifier = FieldModifier.constant
         ..assignment = Code("\"$shortPath\"")
         ..docs = ListBuilder(docsList)));
+
+      if (shortPath.startsWith(_assets_audio)) {
+        String fieldName2 = shortPath.replaceAll(_assets, "").replaceAll(RegExp(r'\s'), "_").replaceAll("/", "_").replaceAll(".", "_");
+        //.replaceFirst(RegExp(r'_'), "", assetsDirWithDividerKey.length - 1);
+        String filedValue2 = shortPath.replaceAll(_assets_audio, "");
+
+        if (fieldName2.endsWith("_")) {
+          fieldName2 = fieldName2.substring(0, fieldName2.length - 1);
+        }
+
+        if (filedValue2.isNotEmpty) {
+          fieldList.add(Field((fieldBuild) => fieldBuild
+            ..static = true
+            ..name = fieldName2
+            ..type = refer("String")
+            ..modifier = FieldModifier.constant
+            ..assignment = Code("\"$filedValue2\"")
+            ..docs = ListBuilder(docsList)));
+        }
+      } else if (shortPath.startsWith(_assets_images)) {
+        String fieldName2 = shortPath.replaceAll(_assets, "").replaceAll(RegExp(r'\s'), "_").replaceAll("/", "_").replaceAll(".", "_");
+        //.replaceFirst(RegExp(r'_'), "", assetsDirWithDividerKey.length - 1);
+        String filedValue2 = shortPath.replaceAll(_assets_images, "");
+
+        if (fieldName2.endsWith("_")) {
+          fieldName2 = fieldName2.substring(0, fieldName2.length - 1);
+        }
+
+        if (filedValue2.isNotEmpty) {
+          fieldList.add(Field((fieldBuild) => fieldBuild
+            ..static = true
+            ..name = fieldName2
+            ..type = refer("String")
+            ..modifier = FieldModifier.constant
+            ..assignment = Code("\"$filedValue2\"")
+            ..docs = ListBuilder(docsList)));
+        }
+      }
     }
 
     final assetsDir = Class((classBuild) => classBuild //生成一个类
@@ -84,13 +126,37 @@ class AssetsDirCommand extends Command<String> {
       ..docs = ListBuilder(["///Code generation, please do not manually modify", "///Assets Dir Reference Class"])
       ..fields.addAll(fieldList));
 
+    ParameterBuilder pathParamMethodBuild = ParameterBuilder()
+      ..name = "fileName"
+      ..type = refer("String");
+
+    Method pathMethod = Method((methodBuilder) => methodBuilder
+      ..name = "filePath"
+      ..requiredParameters.add(pathParamMethodBuild.build())
+      ..returns = refer("String")
+      ..body = Code('''
+              return path.join(this,fileName);
+        '''));
+
+    final stringExtension = Extension((extensionBuilder) => extensionBuilder
+      ..name = "AssetsDirExtension"
+      ..on = refer("String")
+      ..docs = ListBuilder(["///AssetsDir extension"])
+      ..methods = ListBuilder([pathMethod]));
+
+    Directive pathDirective = Directive.import("package:path/path.dart", as: "path");
+
     final emitter = DartEmitter();
 
+    String pathDirectiveStr = DartFormatter().format('${pathDirective.accept(emitter)}');
+    String stringExtensionClassStr = DartFormatter().format('${stringExtension.accept(emitter)}');
     String assetsClassStr = DartFormatter().format('${assetsDir.accept(emitter)}');
 
     File assetsFile = File('$currentDir/lib/generated/assets_dir.dart');
     assetsFile.parent.createSync(recursive: true);
-    assetsFile.writeAsStringSync(assetsClassStr);
+    assetsFile.writeAsStringSync(pathDirectiveStr);
+    assetsFile.writeAsStringSync(assetsClassStr, mode: FileMode.append);
+    assetsFile.writeAsStringSync(stringExtensionClassStr, mode: FileMode.append);
     return null;
   }
 }
